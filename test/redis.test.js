21d19f787b7f61311187bc79b8d3bf447581f3f4
mohammadnuridin <developer.mohammadnuridin@gmail.com>
2024-05-13 16:38:11 +0700

    redis-on-nodejs

---
+++ test/redis.test.js
@@ -0,0 +1,212 @@
+import Redis from "ioredis";
+
+describe("belajar nodejs redis", () => {
+  /**
+   * @type Redis
+   */
+  let redis = null;
+
+  beforeEach(async () => {
+    redis = new Redis({
+      host: "localhost",
+      port: 6379,
+      db: 0,
+    });
+  });
+
+  afterEach(async () => {
+    await redis.quit();
+  });
+
+  it("should can ping", async () => {
+    const pong = await redis.ping();
+    expect(pong).toBe("PONG");
+  });
+
+  it("should support string", async () => {
+    await redis.setex("name", 2, "Eko");
+    let name = await redis.get("name");
+    expect(name).toBe("Eko");
+
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    name = await redis.get("name");
+    expect(name).toBeNull();
+  });
+
+  it("should support list", async () => {
+    await redis.rpush("names", "Eko");
+    await redis.rpush("names", "Kurniawan");
+    await redis.rpush("names", "Khannedy");
+
+    expect(await redis.llen("names")).toBe(3);
+
+    const names = await redis.lrange("names", 0, -1);
+    expect(names).toEqual(["Eko", "Kurniawan", "Khannedy"]);
+
+    expect(await redis.lpop("names")).toBe("Eko");
+    expect(await redis.rpop("names")).toBe("Khannedy");
+
+    expect(await redis.llen("names")).toBe(1);
+
+    await redis.del("names");
+  });
+
+  it("should support set", async () => {
+    await redis.sadd("names", "Eko");
+    await redis.sadd("names", "Eko");
+    await redis.sadd("names", "Kurniawan");
+    await redis.sadd("names", "Kurniawan");
+    await redis.sadd("names", "Khannedy");
+    await redis.sadd("names", "Khannedy");
+
+    expect(await redis.scard("names")).toBe(3);
+
+    const names = await redis.smembers("names");
+    expect(names).toEqual(["Eko", "Kurniawan", "Khannedy"]);
+
+    await redis.del("names");
+  });
+
+  it("should support sorted set", async () => {
+    await redis.zadd("names", 100, "Eko");
+    await redis.zadd("names", 85, "Budi");
+    await redis.zadd("names", 95, "Joko");
+
+    expect(await redis.zcard("names")).toBe(3);
+
+    const names = await redis.zrange("names", 0, -1);
+    expect(names).toEqual(["Budi", "Joko", "Eko"]);
+
+    expect(await redis.zpopmax("names")).toEqual(["Eko", "100"]);
+    expect(await redis.zpopmax("names")).toEqual(["Joko", "95"]);
+    expect(await redis.zpopmax("names")).toEqual(["Budi", "85"]);
+
+    await redis.del("names");
+  });
+
+  it("should support hash", async () => {
+    await redis.hset("user:1", {
+      id: "1",
+      name: "Eko",
+      email: "eko@example.com",
+    });
+
+    const user = await redis.hgetall("user:1");
+
+    expect(user).toEqual({
+      id: "1",
+      name: "Eko",
+      email: "eko@example.com",
+    });
+
+    await redis.del("user:1");
+  });
+
+  it("should support geo point", async () => {
+    await redis.geoadd("sellers", 106.822673, -6.177616, "Toko A");
+    await redis.geoadd("sellers", 106.820646, -6.175366, "Toko B");
+
+    const distance = await redis.geodist("sellers", "Toko A", "Toko B", "KM");
+    expect(distance).toBe(String(0.3361));
+
+    const result = await redis.geosearch(
+      "sellers",
+      "fromlonlat",
+      106.822443,
+      -6.176966,
+      "byradius",
+      5,
+      "km"
+    );
+    expect(result).toEqual(["Toko A", "Toko B"]);
+  });
+
+  it("should support hyper log log", async () => {
+    await redis.pfadd("visitors", "eko", "kurniawan", "khannedy");
+    await redis.pfadd("visitors", "eko", "budi", "joko");
+    await redis.pfadd("visitors", "rully", "budi", "joko");
+
+    const total = await redis.pfcount("visitors");
+    expect(total).toBe(6);
+  });
+
+  it("should support pipeline", async () => {
+    const pipeline = redis.pipeline();
+
+    pipeline.setex("name", 2, "Eko");
+    pipeline.setex("address", 2, "Indonesia");
+
+    await pipeline.exec();
+
+    expect(await redis.get("name")).toBe("Eko");
+    expect(await redis.get("address")).toBe("Indonesia");
+  });
+
+  it("should support transaction", async () => {
+    const transaction = redis.multi();
+
+    transaction.setex("name", 2, "Eko");
+    transaction.setex("address", 2, "Indonesia");
+
+    await transaction.exec();
+
+    expect(await redis.get("name")).toBe("Eko");
+    expect(await redis.get("address")).toBe("Indonesia");
+  });
+
+  it("should support publish to stream", async () => {
+    for (let i = 0; i < 10; i++) {
+      await redis.xadd(
+        "members",
+        "*",
+        "name",
+        `Eko ${i}`,
+        "address",
+        "Indonesia"
+      );
+    }
+  });
+
+  it("should support create consumer and consumer group", async () => {
+    await redis.xgroup("CREATE", "members", "group-1", "0");
+    await redis.xgroup("CREATECONSUMER", "members", "group-1", "consumer-1");
+    await redis.xgroup("CREATECONSUMER", "members", "group-1", "consumer-2");
+  });
+
+  it("should can consumer stream", async () => {
+    const result = await redis.xreadgroup(
+      "GROUP",
+      "group-1",
+      "consumer-1",
+      "COUNT",
+      2,
+      "BLOCK",
+      3000,
+      "STREAMS",
+      "members",
+      ">"
+    );
+    expect(result).not.toBeNull();
+
+    console.info(JSON.stringify(result, null, 2));
+  });
+
+  it("should can subscribe to pubsub", async () => {
+    redis.subscribe("channel-1");
+
+    redis.on("message", (channel, message) => {
+      console.info(
+        `Receive message from channel ${channel} with message ${message}`
+      );
+    });
+
+    // wait 60 seconds
+    await new Promise((resolve) => setTimeout(resolve, 60000));
+  }, 60000);
+
+  it("should can publish to pubsub", async () => {
+    for (let i = 0; i < 10; i++) {
+      await redis.publish("channel-1", `Hello World ${i}`);
+    }
+  });
+});
